fa585a0a273b969117b0a6176977d27fd4015a53
Thorminate <thorwaag11@gmail.com>
2025-04-05 19:32:57 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+from typing import override
+from manim import Scene, Tex, Write, Matrix, DOWN, UP
+
+
+class MathScene(Scene):
+    @override
+    def construct(self):
+        # Create a title with LaTeX
+        title = Tex(r"\textbf{Manim's pretty math!}", font_size=36)
+        _ = title.to_edge(UP)
+        self.play(Write(title))
+        self.wait(1)
+
+        # Section 1: Simple equation
+        equation = Tex(r"E = mc^2", tex_environment="math")
+        _ = equation.next_to(title, DOWN, buff=0.5)
+        self.play(Write(equation))
+        self.wait(1)
+
+        # Section 2: Quadratic equation
+        quadratic_eq = Tex(r"ax^2 + bx + c = 0", tex_environment="math")
+        _ = quadratic_eq.next_to(equation, DOWN, buff=0.5)
+        self.play(Write(quadratic_eq))
+        self.wait(1)
+
+        # Section 3: Quadratic formula
+        quadratic_formula = Tex(
+            r"x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}", tex_environment="math"
+        )
+        _ = quadratic_formula.next_to(quadratic_eq, DOWN, buff=0.5)
+        self.play(Write(quadratic_formula))
+        self.wait(1)
+
+        # Section 4: Matrix
+        matrix = Matrix(
+            [
+                [r"a_{11}", r"a_{12}", r"a_{13}"],
+                [r"a_{21}", r"a_{22}", r"a_{23}"],
+                [r"a_{31}", r"a_{32}", r"a_{33}"],
+            ]
+        )
+        _ = matrix.scale(0.5)
+        _ = matrix.next_to(quadratic_formula, DOWN, buff=0.5)
+        self.play(Write(matrix))
+        self.wait(1)
+
+        # Section 5: Vector
+        vector = Tex(
+            r"\mathbf{v} = \begin{pmatrix} x \\ y \\ z \end{pmatrix}",
+            tex_environment="math",
+        )
+        vector.next_to(matrix, DOWN, buff=0.5)
+        self.play(Write(vector))
+        self.wait(1)
+
+        # Final pause to view the full scene
+        self.wait(2)
